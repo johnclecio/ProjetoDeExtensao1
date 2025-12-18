7727c813b375616162f6a0df095dc39d5eaa0619
John Lima <johncleciofl@gmail.com>
2025-12-18 20:13:26 -0300

    adiciona validação de campos e melhora respostas HTTP

---
+++ lista-de-tarefas/src/main/java/com/projetoextensao/lista_de_tarefas/controller/exception/GlobalExceptionHandler.java
@@ -0,0 +1,29 @@
+package com.projetoextensao.lista_de_tarefas.controller.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationErrors(
+            MethodArgumentNotValidException ex) {
+
+        Map<String, String> erros = new HashMap<>();
+
+        ex.getBindingResult()
+                .getFieldErrors()
+                .forEach(error ->
+                        erros.put(error.getField(), error.getDefaultMessage())
+                );
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(erros);
+    }
+}
